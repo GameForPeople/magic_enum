202e789c5f9bd12d2734f4ed381495c074ace92a
neargye <neargye@gmail.com>
2019-07-11 20:23:32 +0500

    update example

--- example/example.cpp
+++ example/example.cpp
@@ -24,12 +24,12 @@
 
 #include <magic_enum.hpp>
 
-enum Color { RED = -10, BLUE = 0, GREEN = 10 };
+enum class Color : int { RED = -10, BLUE = 0, GREEN = 10 };
 
 int main() {
   // Enum variable to string name.
-  constexpr Color c1 = Color::RED;
-  constexpr auto c1_name = magic_enum::enum_name(c1);
+  Color c1 = Color::RED;
+  auto c1_name = magic_enum::enum_name(c1);
   std::cout << c1_name << std::endl; // RED
 
   // String enum name sequence.
@@ -44,13 +44,13 @@ int main() {
   // String name to enum value.
   constexpr auto c2 = magic_enum::enum_cast<Color>("BLUE");
   if constexpr (c2.has_value() && c2.value() == Color::BLUE) {
-    std::cout << "BLUE = " << c2.value() << std::endl; // BLUE = 0
+    std::cout << "BLUE = " << static_cast<int>(c2.value()) << std::endl; // BLUE = 0
   }
 
   // Integer value to enum value.
   constexpr auto c3 = magic_enum::enum_cast<Color>(10);
   if constexpr (c3.has_value() && c3.value() == Color::GREEN) {
-    std::cout << "GREEN = " << c3.value() << std::endl; // GREEN = 10
+    std::cout << "GREEN = " << magic_enum::enum_integer(c3.value()) << std::endl; // GREEN = 10
   }
 
   // Enum value to integer value.
@@ -83,11 +83,18 @@ int main() {
 
   // Checks whether type is an Unscoped enumeration.
   static_assert(magic_enum::is_unscoped_enum_v<color>);
+  static_assert(!magic_enum::is_unscoped_enum_v<Color>);
   static_assert(!magic_enum::is_unscoped_enum_v<direction>);
 
   // Checks whether type is an Scoped enumeration.
   static_assert(!magic_enum::is_scoped_enum_v<color>);
+  static_assert(magic_enum::is_scoped_enum_v<Color>);
   static_assert(magic_enum::is_scoped_enum_v<direction>);
+
+  // Checks whether type is an Fixed enumeration.
+  static_assert(!magic_enum::is_fixed_enum_v<color>);
+  static_assert(magic_enum::is_fixed_enum_v<Color>);
+  static_assert(magic_enum::is_fixed_enum_v<direction>);
 
   // Enum pair (value enum, string enum name) sequence.
   constexpr auto color_entries = magic_enum::enum_entries<Color>();
